3c86e4f5eb339bf78b0faaea3fa014f09a9e7e39
Leonardo <leogne98@hotmail.com>
2024-03-01 00:42:39 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
-import { useState } from "react";
-
-import Perfil from "./components/Perfil/";
-import Formulario from "./components/Formulario";
-import ReposList from "./components/ReposList";
+import React, { useState } from 'react';
 
 function App() {
-  const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true)
-  const [nomeUsuario, setNomeUsuario] = useState(''); 
-  return (
-    <div>
-      <input type="text" onBlur={(e)=> setNomeUsuario(e.target.value)} />
+    const [altura, setAltura] = useState('');
+    const [peso, setPeso] = useState('');
+    const [imc, setImc] = useState(null);
 
-      {nomeUsuario.length > 4 &&(
-        <>
-          <Perfil nomeUsuario={nomeUsuario}/>
-          <ReposList nomeUsuario={nomeUsuario}/>
-        
-        </>
-      )}
-    {/* {formularioEstaVisivel && (
-      <Formulario/>
-    )}
+    const calcularImc = () => {
+        const alturaMetros = parseFloat(altura) / 100;
+        const imcCalculado = parseFloat(peso) / (alturaMetros * alturaMetros);
+        setImc(imcCalculado.toFixed(2));
+    };
 
-      <button onClick={()=> setFormularioEstaVisivel(!formularioEstaVisivel)} type="button">toggle form</button> */}
-    </div>
-    
-  )
+    return (
+        <div className="form">
+            <h1>Calculadora de IMC</h1>
+            <input type="number" placeholder="Insira sua altura (cm)" value={altura} onChange={(e) => setAltura(e.target.value)}/>
+            <input className='input2' type="number" placeholder="Insira seu peso (kg)" value={peso} onChange={(e) => setPeso(e.target.value)}/>
+            <button onClick={calcularImc}>Calcular IMC</button>
+            {imc && (
+                <p>
+                    Seu IMC é <strong>{imc}</strong>.
+                    {imc < 18.5
+                        ? ' Você está abaixo do peso.'
+                        : imc < 24.9
+                            ? ' Seu peso está normal.'
+                            : ' Você está acima do peso.'}
+                </p>
+            )}
+        </div>
+    );
 }
 
-export default App
+export default App;
